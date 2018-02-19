e640147db12b323281c145f311202f1ada9fb03a
Alban Linard <alban@linard.fr>
2018-02-19 21:23:04 +0100

    Do not run tests if files have changed since less than 1 second.

--- update.sh
+++ update.sh
@@ -1,14 +1,19 @@
 #! /usr/bin/env sh
 
-cd /tmp/sources
+cd /tmp/sources || exit 1
 
 echo "Waiting for changes..."
+last=$(date "+%s")
 while inotifywait \
   --recursive \
   --quiet \
   /tmp/sources
 do
-  luacheck     src/
-  resty_busted src/
-  echo ""
+  current=$(date "+%s")
+  if [ $((current - last)) -ge 1 ]
+  then
+    luacheck     src/
+    resty_busted src/
+    echo ""
+  fi
 done
