8f807348520fa5ec1745eb05fa55ab621b7716b7
Alban Linard <alban@linard.fr>
2018-03-02 11:16:19 +0100

    Upgrade example.

--- src/resty-redis-mapper_spec.lua
+++ src/resty-redis-mapper_spec.lua
@@ -268,10 +268,12 @@ describe ("resty-redis-mapper", function ()
       host = "redis",
       port = 6379,
     }
-
     -- There are some other configuration fields.
+
     -- Create a data type named "type":
     local Type = rrm / "type"
+    -- The type name defines the type of objects.
+    -- It must be unique within the application.
 
     -- Feel free to add any method to the type,
     -- but do **not** change `__index` and `__newindex`,
@@ -302,7 +304,9 @@ describe ("resty-redis-mapper", function ()
     -- This differs from the semantics of tables in the Lua language.
 
     -- Commit all changes done since the module instantiation:
-    assert (rrm ())
+    rrm ()
+    -- If the commit fails, an error is throws.
+    -- There is thus no need to wrap commit within an `assert`.
 
     -- The module instance becomes unusable after commit,
     -- in order to prevent inconsistencies:
@@ -317,7 +321,9 @@ describe ("resty-redis-mapper", function ()
     -- The type must be registered again,
     -- because it is defined per instance of the module:
     Type = rrm / "type"
-    -- In practice, users can create a function that registers all types.
+    -- This statement does not load the methods defined earlier.
+    -- In practice, users can create a function that registers all types,
+    -- and their associated methods.
 
     -- And the object previously created can be loaded using its identifier:
     object = rrm [id_object]
@@ -334,12 +340,31 @@ describe ("resty-redis-mapper", function ()
       local _ = o_rrm / "type"
       local o_object = o_rrm [id_object]
       o_object.c = nil
-      assert (o_rrm ())
+      o_rrm ()
     end
     -- This following commit fails,
     -- because the object has been modified in the block above.
     -- The local copy is thus in a inconsistent state with the reference.
-    assert (rrm ())
+    rrm ()
+    -- Each commit is transactional:
+    -- either everything is updated, or nothing.
+
+    -- `resty-redis-mapper` allows to concurrently read objects.
+    -- There can also be one write to an object, that is read.
+    rrm      = Rrm { host = "redis", port = 6379 }
+    Type     = rrm / "type"
+    object   = rrm [id_object]
+    object.c = "c"
+    do
+      local o_rrm = Rrm { host = "redis", port = 6379 }
+      local _ = o_rrm / "type"
+      local _ = o_rrm [id_object]
+      o_rrm ()
+    end
+    -- This following commit is successful,
+    -- because the object has been modified by only one instance
+    -- of the module.
+    rrm ()
 
     -- It is possible to delete an object using the usual Lua notation:
     rrm    = Rrm { host = "redis", port = 6379 }
@@ -347,7 +372,7 @@ describe ("resty-redis-mapper", function ()
     object = rrm [id_object]
     rrm [object] = nil
     -- rrm [id_object] = nil works as well
-    assert (rrm ())
+    rrm ()
   end)
 
 end)
